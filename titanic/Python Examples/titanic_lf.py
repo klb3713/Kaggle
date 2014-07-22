89dd1c049ff6a5345bf6890021bade7c596ca95a
klb3713 <klbgyx7@gmail.com>
2014-07-22 11:04:29 +0800

    modify titanic

--- titanic/Python Examples/titanic_lf.py
+++ titanic/Python Examples/titanic_lf.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+
+"""
+Kaggle - Titanic
+Problem link: https://www.kaggle.com/c/titanic-gettingStarted
+This is  a logistic regression solution for Kaggle Titanic prediction, achieving accuracy about 83%. The code contains a implementation of logistic regression with L2 regularization.
+The data preprocessing part is left out, we construct the training and testing data with only four features: pclass, sex, age and fare.
+"""
+
 import csv
 from numpy import *
 from logistic_regression import LogisticRegression
@@ -30,16 +38,16 @@ def scale_data(x):
     x = (x - mu) / sigma
     return (x, mu, sigma)
 
-def read_data():
+def read_data(data_file):
     # Data in the file has been preprocessed by eliminating rows with missing values.
-    csv_file_object = csv.reader(open('./data/data.csv', 'rb')) 
-    header = csv_file_object.next()
+    csv_file_object = csv.reader(open(data_file, 'rb'))
+    # header = csv_file_object.next()
     x = []
     for row in csv_file_object:
         x.append(row)
     return array(x, dtype=float64)
 
-if __name__ == '__main__':
+def cross_valid():
     x = read_data()
 
     # Generates training set and cross validation set.
@@ -76,3 +84,35 @@ if __name__ == '__main__':
     p_cv = clf.predict(x_cv)
     print 'Accuracy in training set: %f'% (p == y).mean()
     print 'Accuracy in cv: %f' %  (p_cv == y_cv).mean()
+
+def test():
+    train = read_data('./data/data2.csv')
+    test_x = read_data('./data/test2.csv')
+
+    # Generates training set and test set.
+    train_x = train[:, 1 : :]
+    train_y = train[:, 0]
+
+    train_x = map_feature(train_x)
+    test_x = map_feature(test_x)
+
+    # Feature scaling.
+    train_x, mu, sigma = scale_data(train_x)
+    test_x = (test_x - mu) / sigma
+
+    clf = LogisticRegression(train_x, train_y, 0.1)
+    clf.learn()
+    output = clf.predict(test_x)
+
+    # Write Results to fie
+    seedling=open("./data/logistic_regression.csv", "wb")
+    lr_csv = csv.writer(seedling)
+    lr_csv.writerow(['PassengerId','Survived'])
+    for i in range(len(output)):
+        row = [str(i+892), output[i].astype(uint8)]
+        lr_csv.writerow(row)
+    seedling.close()
+
+if __name__ == '__main__':
+    test()
+
